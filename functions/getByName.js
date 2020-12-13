0ceac8232a640ec2ccae9ef2c9eaff683f9c12c5
EmersonBrSouza <ebssoueu@gmail.com>
2020-12-13 10:17:16 -0300

    ci: netlify.toml

--- functions/getByName.js
+++ functions/getByName.js
@@ -20,7 +20,7 @@ exports.handler = function (event, context, callback) {
         }
         callback(null, { statusCode: 200, body: JSON.stringify({ data: result }) });
     })
-  }).catch((err) => {
+  }).catch(() => {
     callback(null, { statusCode: 500, body: JSON.stringify({ success: false }) });
   });
   
