b50cb7b0eb6a919715e8c6304d8d75d447d5ebad
EmersonBrSouza <ebssoueu@gmail.com>
2020-12-13 11:26:37 -0300

    refactor: getByName

--- functions/getByName.js
+++ functions/getByName.js
@@ -1,28 +1,27 @@
 const mongo = require('mongodb').MongoClient;
 
-exports.handler = function (event, context, callback) {
-  mongo.connect(process.env.DB_CONNECTION_STRING, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  }).then((client) => {
+exports.handler = async function (event, context) {
+  try {
+    const client = await mongo.connect(process.env.DB_CONNECTION_STRING, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
     const db = client.db(process.env.DB_NAME)
     const profile = db.collection('profiles')
-    profile.find({
+    const result = await profile.find({
       name: {
         $regex: new RegExp(`${event.queryStringParameters.name}.*`, 'i')
       }
     }).project({
       hashCode: 0,
       choosedBy: 0
-    }).toArray(function (err, result) {
-        if (err) {
-          callback(null, { statusCode: 500, body: JSON.stringify({ success: false }) });
-        }
-        console.log(result)
-        callback(null, { statusCode: 200, body: JSON.stringify({ data: result }) });
-    })
-  }).catch(() => {
-    callback(null, { statusCode: 500, body: JSON.stringify({ success: false }) });
-  });
-  
+    }).toArray()
+    console.log(result)
+    return { statusCode: 200, body: JSON.stringify({ data: result }) };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ success: false })
+    }
+  }  
 }
